9db3bbbebd353100ea9829ebea55a4fb912366be
Thalisson. <126502431+Thalisson-DEV@users.noreply.github.com>
2025-05-18 15:14:10 -0300

    gitreset
    resetrep

--- index.js
+++
@@ -1,5 +0,0 @@
-// script basico que uso para menu responsivo
-document.querySelector('.menu-toggle').addEventListener('click', () => {
-    document.querySelector('nav ul').classList.toggle('open');
-  });
-  
