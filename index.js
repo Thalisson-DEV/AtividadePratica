95a486531a367f8ae6b3e2381ff8ae8267b898df
Thalisson. <126502431+Thalisson-DEV@users.noreply.github.com>
2025-05-18 15:15:24 -0300

    gitFinal
    pushfinal

---
+++ index.js
@@ -0,0 +1,5 @@
+// script basico que uso para menu responsivo
+document.querySelector('.menu-toggle').addEventListener('click', () => {
+    document.querySelector('nav ul').classList.toggle('open');
+  });
+  
